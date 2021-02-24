ca52e15612119b30abe5aa741ee0c92d8d21b1c3
Justin McMurdie <mcmurdiej@gmail.com>
2021-02-24 16:01:23 -0700

    fixing methods.test.mjs

--- node-test/methods.test.mjs
+++ node-test/methods.test.mjs
@@ -5,31 +5,19 @@ import * as ReactESMProd from "../esm/react.production.min.js";
 
 describe(`All methods exist`, () => {
   it("Dev version has all the same methods as CJS version", () => {
-    // make sure that everything in the ESM bundle also exists in the CJS bundle
-    Object.entries(ReactESMDev, ([key, value]) => {
-      expect(ReactESMDev[key]).to.be.ok;
-      expect(ReactCJSDev[key]).to.be.ok;
-      expect(typeof value).toBe(typeof ReactCJSDev[key]);
-    });
     // make sure that everything in the CJS bundle also exists in the ESM bundle
-    Object.entries(ReactCJSDev, ([key, value]) => {
+    Object.entries(ReactCJSDev).forEach(([key, value]) => {
       expect(ReactESMDev[key]).to.be.ok;
       expect(ReactCJSDev[key]).to.be.ok;
-      expect(typeof value).toBe(typeof ReactESMDev[key]);
+      expect(typeof value).to.equal(typeof ReactESMDev[key]);
     });
   });
   it("Prod version has all the same methods as CJS version", () => {
-    // make sure that everything in the ESM bundle also exists in the CJS bundle
-    Object.entries(ReactESMProd, ([key, value]) => {
-      expect(ReactESMProd[key]).to.be.ok;
-      expect(ReactCJSProd[key]).to.be.ok;
-      expect(typeof value).toBe(typeof ReactCJSProd[key]);
-    });
     // make sure that everything in the CJS bundle also exists in the ESM bundle
-    Object.entries(ReactCJSProd, ([key, value]) => {
+    Object.entries(ReactCJSProd).forEach(([key, value]) => {
       expect(ReactESMProd[key]).to.be.ok;
       expect(ReactCJSProd[key]).to.be.ok;
-      expect(typeof value).toBe(typeof ReactESMProd[key]);
+      expect(typeof value).to.equal(typeof ReactESMProd[key]);
     });
   });
 });
--- src/development.js
+++ src/development.js
@@ -3,11 +3,16 @@ import React from "react/cjs/react.development.js";
 export default React;
 export const __esmModule = true;
 export const {
+  Fragment,
+  StrictMode,
+  Profiler,
+  Suspense,
   Children,
   Component,
   PureComponent,
   __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED,
   cloneElement,
+  createContext,
   createElement,
   createFactory,
   createRef,
