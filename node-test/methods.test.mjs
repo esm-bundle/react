6e1e315d3f14a679a2812a768393b63a8f930280
Justin McMurdie <mcmurdiej@gmail.com>
2021-02-24 16:05:08 -0700

    improving tests so that it helps you identify what is missing

--- node-test/methods.test.mjs
+++ node-test/methods.test.mjs
@@ -4,17 +4,16 @@ import ReactCJSProd from "react/cjs/react.production.min.js";
 import * as ReactESMProd from "../esm/react.production.min.js";
 
 describe(`All methods exist`, () => {
-  it("Dev version has all the same methods as CJS version", () => {
-    // make sure that everything in the CJS bundle also exists in the ESM bundle
-    Object.entries(ReactCJSDev).forEach(([key, value]) => {
+  Object.entries(ReactCJSDev).forEach(([key, value]) => {
+    it(`dev version of ESM bundle should have ${key}`, () => {
       expect(ReactESMDev[key]).to.be.ok;
       expect(ReactCJSDev[key]).to.be.ok;
       expect(typeof value).to.equal(typeof ReactESMDev[key]);
     });
   });
-  it("Prod version has all the same methods as CJS version", () => {
-    // make sure that everything in the CJS bundle also exists in the ESM bundle
-    Object.entries(ReactCJSProd).forEach(([key, value]) => {
+
+  Object.entries(ReactCJSProd).forEach(([key, value]) => {
+    it(`Prod version of ESM bundle should have ${key}`, () => {
       expect(ReactESMProd[key]).to.be.ok;
       expect(ReactCJSProd[key]).to.be.ok;
       expect(typeof value).to.equal(typeof ReactESMProd[key]);
