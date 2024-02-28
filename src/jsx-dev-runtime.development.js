6fd6cf1bf8a12da62648511b415d6d1f164f3be9
vctqs1 <vctqs1@gmail.com>
2024-02-28 20:19:58 +0700

    eexpose JSX runtime

--- src/jsx-dev-runtime.development.js
+++ src/jsx-dev-runtime.development.js
@@ -1,8 +1,9 @@
 import ReactJSXDev from "react/cjs/react-jsx-dev-runtime.development.js";
+import ReactJSX from 'react/jsx-runtime';
 
 export default ReactJSXDev;
 
 export const __esmModule = true;
 
 export const { Fragment, jsxDEV, } = ReactJSXDev;
-export const jsx = jsxDEV;
+export const { jsx, jsxs } = ReactJSX;
